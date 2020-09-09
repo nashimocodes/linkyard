7c35d1fcabf2fd70b988aed4288104003ed353ad
Dhruv Dalsaniya <49398471+acedesyne@users.noreply.github.com>
2020-09-09 14:19:19 +0530

    google analytics is pending

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Thanks Steph for an amazing idea - https://www.instagram.com/p/CE4twpLgf1a/');
+
+console.log(
+	'Feel free to check out the original developer who gave the idea to make own Link service - github: https://github.com/stephjs'
+);
