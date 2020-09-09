6a737b6c5ade5eafb852e0ce8d2bca7d2bced3af
Dhruv Dalsaniya <49398471+acedesyne@users.noreply.github.com>
2020-09-09 17:27:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log('Thanks Steph for an amazing idea - https://www.instagram.com/p/CE4t
 console.log(
 	'Feel free to check out the original developer who gave the idea to make own Link service - github: https://github.com/stephjs'
 );
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CGPXSTF0RS');
