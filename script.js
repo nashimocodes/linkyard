45d2690b45d80674406c8ba439f37f36cdf64232
Piyush Bhangale <bhangalepiyush@gmail.com>
2020-10-01 20:48:03 +0530

    formatting: reformat all code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log('Thanks Steph for an amazing idea - https://www.instagram.com/p/CE4twpLgf1a/');
 
 console.log(
-	'Feel free to check out the original developer who gave the idea to make own Link service - github: https://github.com/stephjs'
+  'Feel free to check out the original developer who gave the idea to make own Link service - github: https://github.com/stephjs'
 );
 
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
 
-  gtag('config', 'G-CGPXSTF0RS');
+gtag('config', 'G-CGPXSTF0RS');
